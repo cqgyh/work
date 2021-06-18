f4249521e47f11508d8c16ea90732b645681467f
cqq <945327638@qq.com>
2021-06-18 15:00:20 +0800

    0618 webpack工程化配置

--- 0617webpack/src/js/index.js
+++ 0617webpack/src/js/index.js
@@ -1,6 +1,11 @@
 import add from './add';
 import person from './person.json';
 
+//将所有的less文件引入到入口文件，让webpack进行统一编译,webpack会将他们写入的js中然后 以js的方式重新写入style标签中重新插入的页面
+import '../less/public.less';
+import '../less/test.less';
 
-add(3,4);
+
+
+add(6,4);
 console.log(person);
